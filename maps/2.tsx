f26f40527f9fc9dd9e2a1f0641895069fadfd1ed
Anastasiashch <118754707+Anastasiashch@users.noreply.github.com>
2023-01-23 15:42:51 +0300

    new_maps

---
+++ maps/2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="2" tilewidth="32" tileheight="32" spacing="5" margin="5" tilecount="729" columns="27">
+ <image source="../../../Downloads/Atlas/base_out_atlas.png" width="1024" height="1024"/>
+</tileset>
